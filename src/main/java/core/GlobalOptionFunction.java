763a5393582e783aa0fe3cefabaf91568072f72c
Koy <koy@ko8e24.top>
2020-12-02 23:27:34 +0800

    [init] basic func.

---
+++ src/main/java/core/GlobalOptionFunction.java
@@ -0,0 +1,17 @@
+package core;
+
+import java.util.Objects;
+import java.util.function.Function;
+
+/**
+ * @Auther Koy  https://github.com/Koooooo-7
+ * @Description
+ */
+@FunctionalInterface
+public interface GlobalOptionFunction<T, R> extends Function<T, R> {
+    @Override
+    default <V> GlobalOptionFunction<T, V> andThen(Function<? super R, ? extends V> after) {
+        Objects.requireNonNull(after);
+        return (T t) -> after.apply(apply(t));
+    }
+}
