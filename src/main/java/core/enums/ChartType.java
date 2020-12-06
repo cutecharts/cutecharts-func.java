ad328f6422ab1bcda57a0eeb84c2351fb562614e
Koy <koy@ko8e24.top>
2020-12-06 17:23:21 +0800

    [refine] code clean.

--- src/main/java/core/enums/ChartType.java
+++ src/main/java/core/enums/ChartType.java
@@ -1,5 +1,22 @@
 package core.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @Auther Koy  https://github.com/Koooooo-7
+ * @Description
+ */
+
+@AllArgsConstructor
+@Getter
 public enum ChartType {
-    Line, Bar
+    LINE("Line"),
+    BAR("Bar"),
+    STACKED_BAR("StackedBar"),
+    RADAR("Radar"),
+    SCATTER("XY"),
+    PIE("Pie");
+
+    private String symbol;
 }
--- src/main/java/core/enums/Position.java
+++ src/main/java/core/enums/Position.java
@@ -1,9 +1,14 @@
 package core.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * @Auther Koy  https://github.com/Koooooo-7
  * @Description
  */
+@Getter
+@AllArgsConstructor
 public enum Position {
     UP_LEFT(1),
     UP_RIGHT(2),
@@ -11,12 +16,4 @@ public enum Position {
     BOTTOM_RIGHT(4);
 
     private int config;
-
-    Position(int config) {
-        this.config = config;
-    }
-
-    public int getConfig() {
-        return config;
-    }
 }
