4dce778e1be2e2779bab33e772b66e34e6c4958c
Koy <koy@ko8e24.top>
2020-12-18 22:15:31 +0800

    test(options): add GlobalOptions Test.

--- src/test/java/core/ChartOptionsTest.java
+++ src/test/java/core/ChartOptionsTest.java
@@ -2,6 +2,7 @@ package core;
 
 import chart.Chart;
 import core.enums.ChartType;
+import opts.GlobalOptions;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -20,4 +21,15 @@ public class ChartOptionsTest {
         Assert.assertEquals("change the mapping chart type", "Bar", chart.getChartType());
     }
 
+    @Test
+    public void testChartGlobalOptions() {
+        Chart chart = GlobalOptions.setTitle("Test")
+                .andThen(GlobalOptions.setYLabel("Y"))
+                .andThen(GlobalOptions.setXLabel("X"))
+                .apply(Chart.chartSupplier(ChartType.LINE).get());
+
+        Assert.assertEquals("title", "Test", chart.getTitle());
+        Assert.assertEquals("XLabel", "X", chart.getXLabel());
+        Assert.assertEquals("YLabel", "Y", chart.getYLabel());
+    }
 }
